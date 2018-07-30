27befc7de651b7c22f2d85462271337ae2d01f2b
nikhild673 <nikhild673@gmail.com>
2018-07-30 20:42:47 +0530

    main

---
+++ app/src/main/java/nick/com/echo/CurrentSongHelper.kt
@@ -0,0 +1,29 @@
+package nick.com.echo
+
+
+/**
+ * Created by ADMIN on 6/19/2018.
+ */
+
+class CurrentSongHelper {
+
+
+    var songArtist: String? = null
+
+    var songTitle: String? = null
+
+    var songPath: String? = null
+
+    var currentPosition: Int = 0
+
+    var songId: Long = 0
+
+    var isPlaying: Boolean? = false
+
+    var isLoop: Boolean = false
+
+    var isShuffle: Boolean = false
+
+    var TrackPosition: Int = 0
+
+}
